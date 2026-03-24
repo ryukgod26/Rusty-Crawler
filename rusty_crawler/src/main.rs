84e0c2e90178f8b6155096afbb530df1f0f762fd
root <root@localhost.localdomain>
2026-03-24 07:40:58 +0000

    Added Position and Renderable Struct

--- rusty_crawler/src/main.rs
+++ rusty_crawler/src/main.rs
@@ -1,10 +1,21 @@
 use wasm_bindgen::prelude::*;
-use rltk::{Rltk,GameState};
+use rltk::{Rltk,GameState,RGB,VirtualKeyCode};
 use specs::prelude::*;
 use std::cmp::{max,min}; 
 use specs_derive::Component;
 
 struct State{}
+#[derive(Component)]
+struct Position{
+    x: i32,
+    y: i32
+}
+
+struct Renderable{
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg:RGB
+}
 
 impl GameState for State{
     fn tick(&mut self,ctx: &mut Rltk){
