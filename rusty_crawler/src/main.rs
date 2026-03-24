46c2e92ba6c2be3fe3d814dae275a31e2f05d88b
root <root@localhost.localdomain>
2026-03-24 08:02:51 +0000

    Added First Entity to the Game with a random Position and Yellow Foreground Color and Black Background Color

--- rusty_crawler/src/main.rs
+++ rusty_crawler/src/main.rs
@@ -4,13 +4,17 @@ use specs::prelude::*;
 use std::cmp::{max,min}; 
 use specs_derive::Component;
 
-struct State{}
+struct State{
+    ecs: World
+}
+
 #[derive(Component)]
 struct Position{
     x: i32,
     y: i32
 }
 
+#[detive(Component)]
 struct Renderable{
     glyph: rltk::FontCharType,
     fg: RGB,
@@ -25,11 +29,25 @@ impl GameState for State{
 }
 
 fn main() -> rltk::BError{
+    let mut gs = State{
+        ecs: World::new()
+    };
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+    
+    gs.ecs.create_entity()
+        .with(Position{x:30, y: 20})
+        .with(Renderable{
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .build();
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
         .with_title("Rusty Crawler")
         .build()?;
-    let gs = State{};
+//    let gs = State{};
     rltk::main_loop(context,gs)
 }
 
