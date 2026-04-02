5654e79ba68a9831e8f0ea9d91a99eed55768221
root <root@localhost.localdomain>
2026-04-02 20:43:37 +0000

    Asking rltk for a viewshed

--- rusty_crawler/src/visibility_system.rs
+++ rusty_crawler/src/visibility_system.rs
@@ -7,8 +7,13 @@ impl<'a> System<'a>  for VisibilitySysten{
     type SystemData = (WriteStorage<'a, Viewshed>,
                         WriteStorage<'a, Position>);
 
-    fn run(&mut self, (mut viewshed, pos): Self::SystemData){
+    fn run(&mut self, data: Self::SystemData){
+        let (map, mut viewshed, pos) = data;
         for(viewshed,pos) in (&mut viewshed,&pos).join(){
+            viewshed.visible_tiles.clear();
+            viewshed.visible_tiles = field_of_view(Point::new(pos.x,pos.y),viewshed.range,&*map);
+            viewshed.visible_tiles.retain(|p| p.x >=0 && p.x < map.width && p.y >= 0&& p.y < map.height);
+
         }
     }
 }
