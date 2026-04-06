1d361cdcf72780aa8edc8f7f8255ac752d926c1c
root <root@localhost.localdomain>
2026-04-06 03:28:23 +0000

    Added a enemy represented by g in every room

--- rusty_crawler/src/main.rs
+++ rusty_crawler/src/main.rs
@@ -74,6 +74,20 @@ fn main() -> rltk::BError{
 
     let map= Map::new_map_rooms_and_corridors();
     let(player_x,player_y) = map.rooms[0].center();
+
+
+    for room in map.rooms.iter().skip(1){
+        let (x,y) = room.center();
+        gc.ecs.create_entity()
+            .with(Position{x,y})
+            .with(Renderable{
+                glyph: rltk::to_cp437('g'),
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK)
+            })
+            .with(Viewshed{visible_tiles: Vec::new(),range: 8, dirty: true})
+            .build();
+
     gs.ecs.insert(map);
 
 
@@ -87,6 +101,8 @@ fn main() -> rltk::BError{
         .with(Player{})
         .with(Viewshed{visible_tiles: Vec::new(),range: 8,dirty: true})
         .build();
+
+    }
 
     // for i in 1..=10{
     //     gs.ecs.create_entity()
