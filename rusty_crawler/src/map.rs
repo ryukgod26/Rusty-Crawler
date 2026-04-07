0ff51409f83d9fc111201d6ba79a6b5a0bd0fe1a
root <root@localhost.localdomain>
2026-04-07 09:55:57 +0000

    Added get_pathing_distance to find tge distance between two indexes by converting them to points first

--- rusty_crawler/src/map.rs
+++ rusty_crawler/src/map.rs
@@ -28,6 +28,13 @@ impl BaseMap for Map{
     fn is_opaque(&self, idx: usize) -> bool{
         self.tiles[idx as usize] == TileType::Wall
     }
+
+    fn get_pathing_distance(&self,idx1: usize,idx2: usize) -> f32{
+        let w = self.width as usize;
+        let p1 = Point::new(idx1 % w, idx1 / w);
+        let p2 = Point::new(idx2 % w, idx2 / w);
+        rltk::DistanceAlg::Pythagoras.distance2d(p1,p2)
+    }
 }
 
 impl Map{
