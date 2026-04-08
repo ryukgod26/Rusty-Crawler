af1060492ce572196773dbbf4cd1a2b8b34a5700
root <root@localhost.localdomain>
2026-04-08 03:52:18 +0000

    Using A star to find a patg from the konster to the player

--- rusty_crawler/src/monster_ai_system.rs
+++ rusty_crawler/src/monster_ai_system.rs
@@ -1,21 +1,37 @@
 use specs::prelude::*;
-use super::{Viewshed,Monster,Name};
+use super::{Viewshed,Monster,Name,Mqp,Position};
 use rltk::{Point,console};
 
 pub struct MonsterAI{}
 
 impl <'a> System<'a> for MonsterAI{
-    type SystemData = ( ReadExpect<'a, Point>,
-                        ReadStorage<'a, Viewshed>,
+    #[allow(clippy::time_complexity)]
+    type SystemData = ( WriteExpect<'a, Map>,
+                        ReadExpect<'a, Point>,
+                        WriteStorage<'a, Viewshed>,
                         ReadStorage<'a, Monster>,
-                        ReadStorage<'a, Name>);
+                        ReadStorage<'a, Name>,
+                        WriteStorage<'a, Position>);
 
     fn run(&mut self, data: Self::SystemData){
-        let (player_pos,viewshed,monster,name) = data;
+        let (mut map,player_pos,mut viewshed,monster,name, mut position) = data;
+    gs.ecs.insert(map);
+    
+    gs.ecs.insert(Point::new{player_x,player_y});
 
-        for (viewshed,_monster) in (&viewshed,&monster).join(){
+        for (mut viewshed,_monster,name,mut pos) in (&mut viewshed,&monster, name, &mut player_pos).join(){
             if viewshed.visible_tiles.contains(&*player_pos){
                 console::log(&format!("{} Monster Speaks",name.name));
+                let path = rltk::a_star_search(
+                    map.xy_index(pos.x,pos.y) as i32,
+                    map.xy_index(player_pos.x,player_pos.y) as i32,
+                    &mut *map
+                    );
+                if path.success && path.steps.len()>1{
+                    pos.x = path.steps[1] as i32 % map.width;
+                    pos.y = path.steps[1] as i32 / map.width;
+                    viewshed.dirty = true;
+                }
             }
 //            console::log("Monster Exists");
         }
